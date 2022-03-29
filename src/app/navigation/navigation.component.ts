3ccebe22b94e6562183737167a295102b798841e
s2100637 <peter.knutas@edu.bc.fi>
2022-03-29 14:36:48 +0300

    add language change option

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'fi', 'se']);
+    translate.setDefaultLang('en');
+
+    const browserLang = translate.getBrowserLang()!;
+    translate.use(browserLang.match(/en|fi|se/) ? browserLang : 'en');
+  }
 
   ngOnInit(): void {
   }
