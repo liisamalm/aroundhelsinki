0659a41e327f1806a62b0bf1333c34af8be1b3e5
metin-gercek <metin.gercek@edu.bc.fi>
2022-03-30 00:57:38 +0300

    add weather forecast and icon

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { WeatherService } from './navigation.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,7 +9,12 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor(public translate: TranslateService) {
+  location = { cityId: 658225, unit: 'metric'};
+  weather: any;
+  iconLink: string = 'http://openweathermap.org/img/w/';
+
+
+  constructor(public translate: TranslateService, public weatherService: WeatherService) {
     translate.addLangs(['en', 'fi', 'se']);
     translate.setDefaultLang('en');
 
@@ -16,7 +22,25 @@ export class NavigationComponent implements OnInit {
     translate.use(browserLang.match(/en|fi|se/) ? browserLang : 'en');
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getWeather(this.location.cityId, this.location.unit)
   }
+
+  getWeather(cityId: any, unit: string) {
+    this.weatherService
+      .getWeather(cityId,unit)
+      .subscribe(res => {
+        this.weather = res;
+        console.log(res);
+      },
+      err => console.log(err));
+  }
+
+  getIcon(icon:string) {
+
+
+    return `${this.iconLink}${icon}.png`;
+  }
+
 
 }
---
+++ src/app/navigation/navigation.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  apiKey: string = '653ec38ccc56d254339a1c306706156a';
+  URI: string = '';
+
+
+  constructor(private http: HttpClient) {
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?id=`;
+
+
+  }
+
+
+  getWeather(cityId: string, unit: string) {
+    return this.http.get(`${this.URI}${cityId}&appid=${this.apiKey}&units=${unit}`);
+  }
+
+
+
+}
