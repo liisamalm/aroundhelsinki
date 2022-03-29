abb8ea5a89ac3e7eede6cc601fdb7e048f708d36
Liisa <s2100647@edu.bc.fi>
2022-03-29 13:38:44 +0300

    add popup module

--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -6,40 +6,31 @@ import * as L from 'leaflet';
   providedIn: 'root'
 })
 export class MarkerService {
- /*  capitals: string = '/assets/data/usa-capitals.geojson'; */
-  capitals: string = '/assets/data/places.json';
+  places: string = '/assets/data/places.json';
 
   constructor(private http: HttpClient) {
   }
 
+  makeMapPopup(data: any): string { 
+    return `` +
+      `<div>Place: ${ data.name.fi }</div>` +
+      `<div>Address: ${ data.location.address.street_address } ${ data.location.address.postal_code } ${ data.location.address.locality } </div>` +
+      `<div>Opening Hours: ${ data.opening_hours.hours[0].weekday_id } ${ data.opening_hours.hours[0].opens } ${ data.opening_hours.hours[0].closes } ${ data.opening_hours.hours[0].open24h }</div>` 
+  }
+
+
   makeCapitalMarkers(map: L.Map): void {
-    this.http.get(this.capitals).subscribe((res: any) => {
+    this.http.get(this.places).subscribe((res: any) => {
       for (const c of res.data) {
-        /* const lon = c.geometry.coordinates[0];
-        const lat = c.geometry.coordinates[1]; */
         const lon = c.location.lon;
         const lat = c.location.lat;
         const marker = L.marker([lat, lon]);
-        
+      
+        marker.bindPopup(this.makeMapPopup(c));
+
         marker.addTo(map);
       }
     });
   }
  
 }
-
-/* 
-/*  makeCapitalMarkers(map: L.Map): void {
-    this.http.get(this.capitals).subscribe((res: any) => {
-      for (const c of res.features) {
-        /* const lon = c.geometry.coordinates[0];
-        const lat = c.geometry.coordinates[1]; 
-        const lon = c.data.location.lat;
-        const lat = c.data.location.lon;
-        const marker = L.marker([lat, lon]);
-          marker.addTo(map);
-      }
-    });
-  }
-       
-*/
