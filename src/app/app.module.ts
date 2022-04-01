02e74d18e4e242e9754e21068048fe92e255b2ab
Liisa <s2100647@edu.bc.fi>
2022-04-01 08:58:54 +0300

    add map to center, still in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 
+
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
 }
