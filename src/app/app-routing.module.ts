d647ef9a3c144165aece3b10a9cdf1123a1a88e2
Liisa <s2100647@edu.bc.fi>
2022-04-08 21:21:04 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' }
+ 
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
