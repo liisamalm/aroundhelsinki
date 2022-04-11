410d1366c614b0531d801ce046b78708467f7d19
metin-gercek <metin.gercek@edu.bc.fi>
2022-04-11 09:06:42 +0300

    add all  listplace

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,11 @@
 import { Component, AfterViewInit, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import {LeafletEvent} from 'leaflet';
+import { LeafletEvent } from 'leaflet';
 import { MarkerService } from './marker.service';
-import {TranslateService} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Places } from './places';
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons'
+import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons';
 import { Distance } from './distance';
 import { placements } from '@popperjs/core';
 import { getPopperClassPlacement } from '@ng-bootstrap/ng-bootstrap/util/positioning';
@@ -30,13 +30,12 @@ L.Marker.prototype.options.icon = iconDefault;
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
-export class MainComponent implements AfterViewInit, OnInit{
-  
+export class MainComponent implements AfterViewInit, OnInit {
   private map: L.Map;
   public location: any = {
-    x: 24.93545, 
+    x: 24.93545,
     y: 60.16952,
-    label: 'test marker'
+    label: 'test marker',
   };
   places: Places[] = [];
   closeResult: string = '';
@@ -44,33 +43,30 @@ export class MainComponent implements AfterViewInit, OnInit{
   faLocationCrosshairs = faLocationCrosshairs;
   placeDistance: any;
 
-  
-  paikanEtaisyys: [] = [];
+  paikanEtaisyys: number[] = [];
+  newDistances : any[] = [];
 
- 
   distance: Distance = {
     placeId: 0,
-    length: 0
+    length: 0,
   };
 
-  
-
-  constructor(private markerService: MarkerService, public translate: TranslateService) { 
+  constructor(
+    private markerService: MarkerService,
+    public translate: TranslateService
+  ) {
     translate.addLangs(['en', 'fi', 'se']);
     translate.setDefaultLang('en');
 
     const browserLang = translate.getBrowserLang()!;
     translate.use(browserLang.match(/en|fi|se/) ? browserLang : 'en');
-
   }
-  
-  private initMap(): void {
 
+  private initMap(): void {
     this.map = L.map('map', {
       center: [60.16952, 24.93545],
       zoom: 12,
     });
-
 
     const provider = new OpenStreetMapProvider();
     const searchControl = new (GeoSearchControl as any)({
@@ -91,58 +87,80 @@ export class MainComponent implements AfterViewInit, OnInit{
 
     tiles.addTo(this.map);
     this.getDistance();
-    
- 
   }
 
   //Etäisyys toimii, mutta näyttäminen ei toimi. Eli kun kokeilee netissä, se näyttää vain ensimmäisen kohdalla etäisyyden. Ja se minkä se näyttää on listan viimeisen.
 
-  getDistance(): void{
-    this.map.on('geosearch/showlocation', (e:  LeafletEvent|any) => {
+  getDistance(): void {
+    this.map.on('geosearch/showlocation', (e: LeafletEvent | any) => {
       const userY = e.location.y;
       const userX = e.location.x;
-      let placeY = "";
-      let placeX = "";
+      let placeY = '';
+      let placeX = '';
       this.markerService.getAllPlaces().subscribe((res: any) => {
         for (const c of res.data) {
           placeX = c.location.lon;
           placeY = c.location.lat;
-          console.log("user locarion: " + userX + " " + userY);
-          console.log("placeX + placeY: " + placeX + " " + placeY);
+          console.log('user locarion: ' + userX + ' ' + userY);
+          console.log('placeX + placeY: ' + placeX + ' ' + placeY);
 
-          let degrees = Math.PI/180;
+          let degrees = Math.PI / 180;
           let dLat = (parseFloat(placeY) - userY) * degrees;
           var dLon = (parseFloat(placeX) - userX) * degrees;
-          var a = Math.pow(Math.sin(dLat/2.0), 2) + Math.cos(userY*degrees) * Math.cos(userX*degrees) * Math.pow(Math.sin(dLon/2.0), 2);
-          var b = 6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-          console.log("Etäisyys " + b + "km");
-          var getElement = document.getElementById("distance");
-          if(getElement){
-            getElement.innerHTML = (b.toFixed(2) + "km");
-            this.placeDistance = b.toFixed(2);
-            console.log(this.placeDistance)
-        }  
+          var a =
+            Math.pow(Math.sin(dLat / 2.0), 2) +
+            Math.cos(userY * degrees) *
+              Math.cos(userX * degrees) *
+              Math.pow(Math.sin(dLon / 2.0), 2);
+          var b = 6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+          console.log('Etäisyys ' + b + 'km');
+          var getElement = document.getElementById('distance');
+          if (getElement) {
+            // getElement.innerHTML = b.toFixed(2) + 'km';
+            this.placeDistance = String(b.toFixed(2));
+            console.log(this.placeDistance);
           }
-          console.log(this.placeDistance)
-          
-      });
+          this.paikanEtaisyys.push(this.placeDistance);
+          console.log(this.paikanEtaisyys);
 
+          this.newDistances = this.paikanEtaisyys;
+          console.log(this.newDistances);
+          console.log(typeof(this.newDistances.forEach));
+
+        }
+
+        // var newDistances : any[] = [];
+        // newDistances = this.paikanEtaisyys;
+
+        // console.log(newDistances);
+
+        // var getElement = document.getElementById('distance');
+        // for(let i = 0; i < newDistances.length; i++) {
+        //   this.placeDistance = newDistances[i];
+        //   console.log(this.placeDistance);
+        //   getElement!.innerHTML = newDistances[i]
+        // }
+
+
+
+
+        // console.log(typeof this.paikanEtaisyys);
+      });
     });
   }
-  
+
   ngAfterViewInit(): void {
     this.initMap();
     this.markerService.makePlaceMarkers(this.map);
   }
 
-
   ngOnInit(): void {
     this.getAllPlaces();
-   }
+  }
 
-  getAllPlaces(): void{
+  getAllPlaces(): void {
     this.markerService.getAllPlaces().subscribe((res: Places) => {
-    this.places.push(res);
+      this.places.push(res);
     });
   }
 }
