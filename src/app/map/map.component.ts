25473d741b02d2edef79601631c14762f7d3ad45
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-04-25 14:22:30 +0300

    map work but not distance

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import { PopupComponent } from '../popup/popup.component';
+import { LeafletEvent, MarkerClusterGroup } from 'leaflet';
+import { ApiService } from '../services/api.service';
+import { Injector, ApplicationRef, ComponentFactoryResolver, Type } from '@angular/core';
+
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
 
 
 @Component({
@@ -11,9 +32,11 @@ export class MapComponent implements OnInit {
   static map: L.Map;
   // static map: any;
 
-  constructor() { }
+  constructor(public apiService: ApiService, private resolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector) { }
 
-  mapInit(){
+  mapInit() {
     MapComponent.map = L.map('map', {
       center: [60.16952, 24.93545],
       zoom: 12,
@@ -30,8 +53,63 @@ export class MapComponent implements OnInit {
     tiles.addTo(MapComponent.map);
   }
 
-  ngOnInit(): void {
+  mainPageMap() {
     this.mapInit();
+    const provider = new OpenStreetMapProvider();
+    const searchControl = new (GeoSearchControl as any)({
+      provider: provider,
+      autoClose: true,
+    });
+    MapComponent.map.addControl(searchControl);
+
+  }
+
+  makeMapPopup(data: any): any {
+    let markerPopup: any = this.compilePopup(PopupComponent, (c: any) => {
+      (c.instance.place = data.name.fi),
+        (c.instance.address = data.location.address.street_address),
+        (c.instance.postalCode = data.location.address.postal_code),
+        (c.instance.locality = data.location.address.locality),
+        (c.instance.placeUrl = data.info_url),
+        (c.instance.ownPage = data.id)
+    });
+    return markerPopup;
+  }
+
+  makePlaceMarkers(map: L.Map) {
+    const markerCluster = new MarkerClusterGroup();
+    this.apiService.httpPlaceMarker().subscribe((res: any) => {
+      for (const c of res.data) {
+        const lon = c.location.lon;
+        const lat = c.location.lat;
+
+        const marker = L.marker([lat, lon], { icon: iconDefault }).bindPopup(this.makeMapPopup(c));
+
+        markerCluster.addLayer(marker);
+      }
+      map.addLayer(markerCluster);
+    });
+
+  }
+
+  private compilePopup(component: any, onAttach: any): any {
+    const compFactory: any = this.resolver.resolveComponentFactory(component);
+    let compRef: any = compFactory.create(this.injector);
+
+    if (onAttach)
+      onAttach(compRef);
+
+    this.appRef.attachView(compRef.hostView);
+    compRef.onDestroy(() => this.appRef.detachView(compRef.hostView));
+
+    let div = document.createElement('div');
+    div.appendChild(compRef.location.nativeElement);
+    return div;
+  }
+
+  ngOnInit(): void {
+    this.mainPageMap();
+    this.makePlaceMarkers(MapComponent.map);
   }
 
 }
