e9b8d3dd32f42b4ad5b10cc643c6f83147e7c8b3
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-04-27 09:17:02 +0300

    remove empty rows

--- src/app/interfaces/dtos.ts
+++ src/app/interfaces/dtos.ts
@@ -3,7 +3,6 @@ export interface AllDTO{
     event: EventDTO[];
     activity: ActivityDTO[];
 }
-
 export interface PlaceDTO {
     id: string;
     name:  NameClass;
@@ -11,16 +10,13 @@ export interface PlaceDTO {
     latlon: LatLon;
     distance: number;
 }
-
 export interface EventDTO {
     id: string;
     name:  NameClass;
     address: Address;
     latlon: LatLon;
     distance: number;
-
 }
-
 export interface ActivityDTO {
     id: string;
     name:  NameClass;
@@ -28,20 +24,17 @@ export interface ActivityDTO {
     latlon: LatLon;
     distance: number;
 }
-
 export interface NameClass {
     fi: string;
     en: null | string;
     sv: null | string;
     zh: null | string;
 }
-
 export interface Address {
     street_address: null | string;
     postal_code:    null | string;
     locality:       string | null;
 }
-
 export interface LatLon {
     lat: number;
     lon: number;
