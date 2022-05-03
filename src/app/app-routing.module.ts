4948001a87c0d912b15647e4d71a8ba838084e41
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-05-03 12:18:43 +0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
 import { PlaceDetailComponent } from './place-detail/place-detail.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'places', component: MainComponent },
   { path: 'events', component: MainComponent },
   { path: 'activities', component: MainComponent },
-  { path: 'main/detail/:id', component: PlaceDetailComponent },
+  { path: 'main/detail/:id', component: EventDetailComponent },
+  { path: 'main/event/:id', component: EventDetailComponent },
   { path: '', redirectTo: '/main', pathMatch: 'full' },
 
 ];
