6fd5028eea7cb639d83cfe9aae24bf6d03ddf107
metin-gercek <metin.gercek@edu.bc.fi>
2022-05-04 12:36:34 +0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,16 @@ import { ActivityDetailComponent } from './activity-detail/activity-detail.compo
 import { EventDetailComponent } from './event-detail/event-detail.component';
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent },
+  { path: '', component: MainComponent },
   { path: 'all', component: MainComponent },
   { path: 'places', component: MainComponent },
   { path: 'events', component: MainComponent },
   { path: 'activities', component: MainComponent },
-  { path: 'main/activity/:id', component: ActivityDetailComponent },
-  { path: 'main/place/:id', component: PlaceDetailComponent },
-  { path: 'main/event/:id', component: EventDetailComponent },
-  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'activity/:id', component: ActivityDetailComponent },
+  { path: 'place/:id', component: PlaceDetailComponent },
+  { path: 'places/place/:id', component: PlaceDetailComponent },
+  { path: 'event/:id', component: EventDetailComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 
 ];
 
