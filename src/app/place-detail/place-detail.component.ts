dfc8b2272da0d31a43ce25fa4f3c4bbacfba8ba0
s2100637 <peter.knutas@edu.bc.fi>
2022-05-05 09:37:04 +0300

    add place detail marker to place-detail component

--- src/app/place-detail/place-detail.component.ts
+++ src/app/place-detail/place-detail.component.ts
@@ -7,7 +7,7 @@ import * as L from 'leaflet';
 import { TranslateService } from '@ngx-translate/core';
 
 const iconPlace = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  iconUrl: '../assets/images/marker_place.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
