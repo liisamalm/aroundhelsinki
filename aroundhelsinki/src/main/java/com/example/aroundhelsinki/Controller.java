82b127f68d9fa37306097e9cd42bb5c3d7638be0
s2100637 <peter.knutas@edu.bc.fi>
2022-05-13 13:11:01 +0300

    remove some more code

--- aroundhelsinki/src/main/java/com/example/aroundhelsinki/Controller.java
+++ aroundhelsinki/src/main/java/com/example/aroundhelsinki/Controller.java
@@ -3,7 +3,6 @@ package com.example.aroundhelsinki;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
