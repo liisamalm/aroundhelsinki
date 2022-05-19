937d88e4c605a13f615a093415b74b53784b8927
s2100637 <peter.knutas@edu.bc.fi>
2022-05-19 15:02:31 +0300

    change heroku address

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "https://aroundhelsinki-test.herokuapp.com"
+  baseUrl: "https://around-helsinki.herokuapp.com"
 };
--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://aroundhelsinki-test.herokuapp.com"
+  baseUrl: "https://around-helsinki.herokuapp.com"
 };
 
 /*
